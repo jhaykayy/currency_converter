10cba3c14fe791234ebd59005b915fbe4d40ccc5
joshua <jkweknow@gmail.com>
2025-01-15 17:23:02 +0100

    border radius and keyboardtype

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -4,34 +4,48 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+  Widget  build(BuildContext context) {
+    final border =  OutlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.black,
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(60),
+    );
+
+    return Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               '0',
               style: TextStyle(
                   fontSize: 45,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black),
+                  color: Colors.white),
             ),
-            TextField(
-              style: TextStyle(
+             TextField(
+              style: const TextStyle(
                 color: Colors.black,
               ),
               decoration: InputDecoration(
                 hintText: "Please enteer amount in USD",
-                hintStyle: TextStyle(
+                hintStyle: const TextStyle(
                   color: Colors.black,
                 ),
-                prefixIcon: Icon(Icons.monetization_on_outlined),
+                prefixIcon: const Icon(Icons.monetization_on_outlined),
                 prefixIconColor: Colors.black,
                 filled: true,
                 fillColor: Colors.white,
+                focusedBorder: border,
+                enabledBorder: border,
               ),
+              keyboardType: const TextInputType.numberWithOptions(
+                decimal: true,
+              )
             ),
           ],
         ),
